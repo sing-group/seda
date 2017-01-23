c14449d6e1ddadda19a3da73b7062ec92f74b0fe
Miguel Reboiro-Jato <mrjato@uvigo.es>
2017-01-23 18:08:14 +0100

    Adds a working dialog when processing dataset
    A new modal dialog is now shown when processing a dataset, that informs that
    the application is working. This modal also locks the application, so that the
    user can't use it while it is working.

--- src/main/java/org/sing_group/seda/gui/SedaPanel.java
+++ src/main/java/org/sing_group/seda/gui/SedaPanel.java
@@ -1,17 +1,24 @@
 package org.sing_group.seda.gui;
 
+import static java.util.Objects.requireNonNull;
+
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.stream.Stream;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JDialog;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
+import javax.swing.SwingConstants;
 import javax.swing.SwingUtilities;
+import javax.swing.SwingWorker;
 
 import org.sing_group.seda.datatype.DatatypeFactory;
 import org.sing_group.seda.io.DatasetProcessor;
@@ -20,6 +27,8 @@ import org.sing_group.seda.transformation.dataset.MSADatasetTransformation;
 
 public class SedaPanel extends JTabbedPane {
 	private static final long serialVersionUID = 1L;
+	
+	private static final ImageIcon ICON_WORKING = new ImageIcon(SedaPanel.class.getResource("image/working.gif"));
 
 	private final PathSelectionPanel panelPathSelection;
 	private final OutputConfigurationPanel panelOutputConfig;
@@ -60,37 +69,65 @@ public class SedaPanel extends JTabbedPane {
 	}
 	
 	private void generate() {
-		final PathSelectionModel pathsModel = this.panelPathSelection.getModel();
-		final OutputConfigurationModel outputModel = this.panelOutputConfig.getModel();
-		final TransformationsConfigurationModel transformationsModel = this.panelTransformations.getModel();
+		final JDialog dialog = new JDialog((JFrame) SwingUtilities.getAncestorOfClass(JFrame.class, this), "Executing task", true);
 		
-		final Stream<Path> paths = pathsModel.getSelectedPaths()
-			.map(Paths::get);
+		dialog.setContentPane(new JLabel("Running task...", ICON_WORKING, SwingConstants.LEADING));
+		dialog.pack();
+		dialog.setLocationRelativeTo(null);
 		
-		final Path output = outputModel.getOutputDirectory();
-		final int groupSize = outputModel.isSplitInSubdirectories() ?
-			outputModel.getSubdirectorySize() : 0;
+		new CustomSwingWorker(() -> {
+			final PathSelectionModel pathsModel = this.panelPathSelection.getModel();
+			final OutputConfigurationModel outputModel = this.panelOutputConfig.getModel();
+			final TransformationsConfigurationModel transformationsModel = this.panelTransformations.getModel();
 			
-		final MSADatasetTransformation transformation = transformationsModel.toTransformation(this.factory);
-		
-		try {
-			this.processor.process(paths, output, transformation, groupSize);
+			final Stream<Path> paths = pathsModel.getSelectedPaths()
+				.map(Paths::get);
 			
-			JOptionPane.showMessageDialog(
-				this,
-				"Transformation completed without any error. You can find the resulting dataset in: " + output.toString(),
-				"Transformation Completed",
-				JOptionPane.INFORMATION_MESSAGE
-			);
-		} catch (Throwable e) {
-			e.printStackTrace();
-			JOptionPane.showMessageDialog(
-				this,
-				"Error transforming dataset: " + e.getMessage(),
-				"Transformation Error",
-				JOptionPane.ERROR_MESSAGE
-			);
+			final Path output = outputModel.getOutputDirectory();
+			final int groupSize = outputModel.isSplitInSubdirectories() ?
+				outputModel.getSubdirectorySize() : 0;
+				
+			final MSADatasetTransformation transformation = transformationsModel.toTransformation(this.factory);
+			
+			try {
+				this.processor.process(paths, output, transformation, groupSize);
+				
+				dialog.dispose();
+				JOptionPane.showMessageDialog(
+					this,
+					"Transformation completed without any error. You can find the resulting dataset in: " + output.toString(),
+					"Transformation Completed",
+					JOptionPane.INFORMATION_MESSAGE
+				);
+			} catch (Throwable e) {
+				e.printStackTrace();
+				dialog.dispose();
+				JOptionPane.showMessageDialog(
+					this,
+					"Error transforming dataset: " + e.getMessage(),
+					"Transformation Error",
+					JOptionPane.ERROR_MESSAGE
+				);
+			}
+		}).execute();
+		
+		dialog.setVisible(true);
+	}
+	
+	private static class CustomSwingWorker extends SwingWorker<Void, Void> {
+		private final Runnable task;
+		
+		public CustomSwingWorker(Runnable task) {
+			this.task = requireNonNull(task);
 		}
+		
+		@Override
+		protected Void doInBackground() throws Exception {
+			this.task.run();
+			
+			return null;
+		}
+		
 	}
 	
 	public static void main(String[] args) {
