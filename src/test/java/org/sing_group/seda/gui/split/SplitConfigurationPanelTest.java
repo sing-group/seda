c7e571ca30ef54dacdba0d9840ac6b4088a00b1f
Hugo López Fernández <hlfernandez@uvigo.es>
2017-11-03 11:53:45 +0000

    Refactor GUI tests

--- src/test/java/org/sing_group/seda/gui/split/SplitConfigurationPanelTest.java
+++ src/test/java/org/sing_group/seda/gui/split/SplitConfigurationPanelTest.java
@@ -1,22 +1,12 @@
 package org.sing_group.seda.gui.split;
 
 import org.sing_group.seda.gui.TestGuiUtils;
-import org.sing_group.seda.plugin.spi.TransformationChangeEvent;
-import org.sing_group.seda.plugin.spi.TransformationChangeListener;
 
 public class SplitConfigurationPanelTest {
 
   public static void main(String[] args) {
     SplitConfigurationPanel panel = new SplitConfigurationPanel();
-    panel.getModel().addTransformationChangeListener(
-      new TransformationChangeListener() {
-
-        @Override
-        public void onTransformationChange(TransformationChangeEvent event) {
-          System.err.println("Transformation changed: " + event.getType());
-        }
-      }
-    );
+    panel.getModel().addTransformationChangeListener(TestGuiUtils.TRANSFORMATION_CHANGE_LISTENER);
     TestGuiUtils.showComponent(panel);
   }
 }
