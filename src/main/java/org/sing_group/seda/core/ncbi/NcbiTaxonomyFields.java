6abe8fa61c33d1e84e5758bcea32b42787c269a7
Hugo López Fernández <hlfernandez@uvigo.es>
2018-01-12 09:58:11 +0000

    Make Ncbi datatypes serializable

--- src/main/java/org/sing_group/seda/core/ncbi/NcbiTaxonomyFields.java
+++ src/main/java/org/sing_group/seda/core/ncbi/NcbiTaxonomyFields.java
@@ -1,8 +1,10 @@
 package org.sing_group.seda.core.ncbi;
 
+import java.io.Serializable;
+
 import org.sing_group.seda.util.StringUtils;
 
-public enum NcbiTaxonomyFields {
+public enum NcbiTaxonomyFields implements Serializable {
   SUPERKINGDOM, KINGDOM, PHYLUM, SUBPHYLUM, CLASS, SUPERORDER, ORDER, 
   SUBORDER, INFRAORDER, PARVORDER, SUPERFAMILY, FAMILY, SUBFAMILY, GENUS;
 
--- src/main/java/org/sing_group/seda/core/ncbi/NcbiTaxonomyInfo.java
+++ src/main/java/org/sing_group/seda/core/ncbi/NcbiTaxonomyInfo.java
@@ -1,9 +1,11 @@
 package org.sing_group.seda.core.ncbi;
 
+import java.io.Serializable;
 import java.util.Map;
 import java.util.Optional;
 
-public class NcbiTaxonomyInfo {
+public class NcbiTaxonomyInfo implements Serializable {
+  private static final long serialVersionUID = 1L;
 
   private Map<NcbiTaxonomyFields, String> values;
 
