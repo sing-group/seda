5e770ca7804e813b48cfb08a2813b6baad850aa3
Hugo López Fernández <hlfernandez@uvigo.es>
2018-01-22 11:54:27 +0000

    Add missing taxonomy terms

--- src/main/java/org/sing_group/seda/core/ncbi/NcbiTaxonomyFields.java
+++ src/main/java/org/sing_group/seda/core/ncbi/NcbiTaxonomyFields.java
@@ -5,13 +5,13 @@ import java.io.Serializable;
 import org.sing_group.seda.util.StringUtils;
 
 public enum NcbiTaxonomyFields implements Serializable {
-  SUPERKINGDOM, KINGDOM, PHYLUM, SUBPHYLUM, CLASS, SUPERORDER, ORDER, 
-  SUBORDER, INFRAORDER, PARVORDER, SUPERFAMILY, FAMILY, SUBFAMILY, GENUS;
+  SUPERKINGDOM, KINGDOM, PHYLUM, SUBPHYLUM, INFRACLASS, SUPERCLASS, SUBCLASS, CLASS,
+  SUPERORDER, ORDER, SUBORDER, INFRAORDER, PARVORDER, SUPERFAMILY, FAMILY, SUBFAMILY, GENUS;
 
   public String nodeTitle() {
     return this.toString().toLowerCase();
   }
-  
+
   @Override
   public String toString() {
     return StringUtils.capitalize(super.toString());
