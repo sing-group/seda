31445258b36e9d1017a3649727e01d08d00c1ac2
Hugo López-Fernández <hlfernandez@uvigo.es>
2018-12-10 12:43:51 +0100

    Change version to 0.5.0

--- seda-docs/source/conf.py
+++ seda-docs/source/conf.py
@@ -54,7 +54,7 @@ author = u'Jorge Vieira, Miguel Reboiro-Jato and Hugo López-Fernández'
 # built documents.
 #
 # The short X.Y version.
-version = u'0.5.0-SNAPSHOT'
+version = u'0.5.0'
 # The full version, including alpha/beta/rc tags.
 release = u'0.5'
 
