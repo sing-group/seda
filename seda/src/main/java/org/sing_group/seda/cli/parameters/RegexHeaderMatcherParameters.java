43a5997e35645ac3913965e68867b4c793f8fb1f
David Vila <dvfernandez@esei.uvigo.es>
2022-05-05 14:59:34 +0200

    Adds missing final modifier to options' attributes

--- seda/src/main/java/org/sing_group/seda/cli/parameters/RegexHeaderMatcherParameters.java
+++ seda/src/main/java/org/sing_group/seda/cli/parameters/RegexHeaderMatcherParameters.java
@@ -8,12 +8,12 @@
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/gpl-3.0.html>.
@@ -52,25 +52,27 @@ import es.uvigo.ei.sing.yacli.command.parameter.Parameters;
 
 public class RegexHeaderMatcherParameters {
 
-  public static DefaultValuedStringOption OPTION_STRING =
-    new DefaultValuedStringOption(PARAM_REGEX_STRING_NAME, PARAM_REGEX_STRING_SHORT_NAME, PARAM_REGEX_STRING_HELP, ".*");
+  public static final DefaultValuedStringOption OPTION_STRING =
+    new DefaultValuedStringOption(
+      PARAM_REGEX_STRING_NAME, PARAM_REGEX_STRING_SHORT_NAME, PARAM_REGEX_STRING_HELP, ".*"
+    );
 
-  public static FlagOption OPTION_QUOTE_PATTERN =
+  public static final FlagOption OPTION_QUOTE_PATTERN =
     new FlagOption(
       PARAM_REGEX_QUOTE_PATTERN_NAME, PARAM_REGEX_QUOTE_PATTERN_SHORT_NAME, PARAM_REGEX_QUOTE_PATTERN_HELP
     );
 
-  public static IntegerDefaultValuedStringConstructedOption OPTION_GROUP =
+  public static final IntegerDefaultValuedStringConstructedOption OPTION_GROUP =
     new IntegerDefaultValuedStringConstructedOption(
       PARAM_REGEX_GROUP_NAME, PARAM_REGEX_GROUP_SHORT_NAME, PARAM_REGEX_GROUP_HELP, 0
     );
 
-  public static FlagOption OPTION_CASE_SENSITIVE =
+  public static final FlagOption OPTION_CASE_SENSITIVE =
     new FlagOption(
       PARAM_REGEX_CASE_SENSITIVE_NAME, PARAM_REGEX_CASE_SENSITIVE_SHORT_NAME, PARAM_REGEX_CASE_SENSITIVE_HELP
     );
 
-  public static DefaultValuedStringOption OPTION_HEADER_TARGET =
+  public static final DefaultValuedStringOption OPTION_HEADER_TARGET =
     new DefaultValuedStringOption(
       PARAM_REGEX_HEADER_TARGET_NAME, PARAM_REGEX_HEADER_TARGET_SHORT_NAME, PARAM_REGEX_HEADER_TARGET_HELP,
       HeaderTarget.NAME.name().toLowerCase()
@@ -106,10 +108,11 @@ public class RegexHeaderMatcherParameters {
       );
     }
 
-    RegexConfiguration regexConfiguration = new RegexConfiguration(
-      parameters.hasFlag(OPTION_CASE_SENSITIVE), parameters.getSingleValue(OPTION_GROUP),
-      parameters.hasFlag(OPTION_QUOTE_PATTERN)
-    );
+    RegexConfiguration regexConfiguration =
+      new RegexConfiguration(
+        parameters.hasFlag(OPTION_CASE_SENSITIVE), parameters.getSingleValue(OPTION_GROUP),
+        parameters.hasFlag(OPTION_QUOTE_PATTERN)
+      );
 
     return new RegexHeaderMatcher(regex, headerTarget, regexConfiguration);
   }
