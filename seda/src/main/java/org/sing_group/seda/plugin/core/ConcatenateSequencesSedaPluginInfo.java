30d33b51f8d3ed0cc99d63d800bad50c45d52177
David Vila <dvfernandez@esei.uvigo.es>
2022-05-09 18:01:53 +0200

    Moves header matcher's configuration to the correct class
    Now sequence matching parameter's info is on the RegexHeaderMatcherInfo with the
     others parameters' info

--- seda/src/main/java/org/sing_group/seda/plugin/core/ConcatenateSequencesSedaPluginInfo.java
+++ seda/src/main/java/org/sing_group/seda/plugin/core/ConcatenateSequencesSedaPluginInfo.java
@@ -8,24 +8,18 @@
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/gpl-3.0.html>.
  * #L%
  */
 package org.sing_group.seda.plugin.core;
-
-import static java.util.Arrays.asList;
-import static java.util.stream.Collectors.joining;
-import static java.util.stream.Stream.of;
-
-import org.sing_group.seda.core.filtering.HeaderFilteringConfiguration;
 
 public class ConcatenateSequencesSedaPluginInfo extends RegexHeaderMatcherInfo {
   public static final String NAME = "Concatenate sequences";
@@ -45,19 +39,4 @@ public class ConcatenateSequencesSedaPluginInfo extends RegexHeaderMatcherInfo {
   public static final String PARAM_MERGE_HELP =
     "Whether the sequence descriptions must be added to the concatenated sequences or not.";
   public static final String PARAM_MERGE_HELP_GUI = toHtml(PARAM_MERGE_HELP);
-
-  public static final String PARAM_SEQUENCE_MATCHING_NAME = "sequence-matching";
-  public static final String PARAM_SEQUENCE_MATCHING_SHORT_NAME = "sm";
-  public static final String PARAM_SEQUENCE_MATCHING_DESCRIPTION = "Sequence matching mode";
-  public static final String PARAM_SEQUENCE_MATCHING_HELP =
-    "The sequence matching mode. One of: "
-      + of(HeaderFilteringConfiguration.FilterType.values()).map(HeaderFilteringConfiguration.FilterType::name)
-        .map(String::toLowerCase).collect(joining(", ", "", "."))
-      + "\n\t\tSequence name means that the sequences are concatenated if they have the same sequence names (identifiers).\n"
-      + "\t\tRegular expression means sequences are concatenade by matching headers using the configuration specified below.\n\t\t";
-  public static final String PARAM_SEQUENCE_MATCHING_HELP_GUI =
-    toHtml(
-      PARAM_SEQUENCE_MATCHING_HELP, asList("Sequence name", "Regular expression"),
-      asList("Sequence name", "Regular expression"), true
-    );
 }
--- seda/src/main/java/org/sing_group/seda/plugin/core/RegexHeaderMatcherInfo.java
+++ seda/src/main/java/org/sing_group/seda/plugin/core/RegexHeaderMatcherInfo.java
@@ -8,12 +8,12 @@
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/gpl-3.0.html>.
@@ -23,13 +23,29 @@ package org.sing_group.seda.plugin.core;
 
 import static java.util.Arrays.asList;
 import static java.util.stream.Collectors.joining;
+import static java.util.stream.Stream.of;
 
 import java.util.Collections;
 import java.util.stream.Stream;
 
+import org.sing_group.seda.core.filtering.HeaderFilteringConfiguration;
 import org.sing_group.seda.core.rename.HeaderTarget;
 
 public class RegexHeaderMatcherInfo extends AbstractInfo {
+  public static final String PARAM_SEQUENCE_MATCHING_NAME = "sequence-matching";
+  public static final String PARAM_SEQUENCE_MATCHING_SHORT_NAME = "sm";
+  public static final String PARAM_SEQUENCE_MATCHING_DESCRIPTION = "Sequence matching mode";
+  public static final String PARAM_SEQUENCE_MATCHING_HELP =
+    "The sequence matching mode. One of: "
+      + of(HeaderFilteringConfiguration.FilterType.values()).map(HeaderFilteringConfiguration.FilterType::name)
+        .map(String::toLowerCase).collect(joining(", ", "", "."))
+      + "\n\t\tSequence name means that the sequences are concatenated if they have the same sequence names (identifiers).\n"
+      + "\t\tRegular expression means sequences are concatenade by matching headers using the configuration specified below.\n\t\t";
+  public static final String PARAM_SEQUENCE_MATCHING_HELP_GUI =
+    toHtml(
+      PARAM_SEQUENCE_MATCHING_HELP, asList("Sequence name", "Regular expression"),
+      asList("Sequence name", "Regular expression"), true
+    );
   public static final String PARAM_REGEX_STRING_NAME = "string-match";
   public static final String PARAM_REGEX_STRING_SHORT_NAME = "rs";
   public static final String PARAM_REGEX_STRING_DESCRIPTION = "String to match";
